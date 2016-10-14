6841a07675d3932446b5eada0d612a3841f511b0
gmatteo <gmatteo@gmail.com>
2016-10-14 16:16:20 +0200

    Add headerpad_max_install_name

--- recipes/abinit/build.sh
+++ recipes/abinit/build.sh
@@ -1,80 +1,89 @@
 #!/usr/bin/env bash
 #https://github.com/conda-forge/fftw-feedstock/blob/ad8bc8c5661447db646ff3c48fed3dd4a23edc5a/recipe/build.sh
 
-# For MAC OSx see:
-# https://github.com/ContinuumIO/anaconda-issues/issues/739
-
 # Depending on our platform, shared libraries end with either .so or .dylib
-#if [[ `uname` == 'Darwin' ]]; then
-#    export LIBRARY_SEARCH_VAR=DYLD_FALLBACK_LIBRARY_PATH
-#    export DYLIB_EXT=dylib
-#    export CC=clang
-#    export CXX=clang++
-#    export CXXFLAGS="-stdlib=libc++"
-#    export CXX_LDFLAGS="-stdlib=libc++"
-#else
-#    export LIBRARY_SEARCH_VAR=LD_LIBRARY_PATH
-#    export DYLIB_EXT=so
-#    export CC=gcc
-#    export CXX=g++
-#fi
-
-
-if [[ $(uname) == Darwin ]]; then
-  export LIBRARY_SEARCH_VAR=DYLD_FALLBACK_LIBRARY_PATH
-elif [[ $(uname) == Linux ]]; then
-  export LIBRARY_SEARCH_VAR=LD_LIBRARY_PATH
+if [[ `uname` == 'Darwin' ]]; then
+    export DYLIB_EXT=dylib
+    #export CC=clang
+    export CC=gcc-5
+    export FC=gfortran-5
+    export LDD="otool -L"
+else
+    export DYLIB_EXT=so
+    export CC=gcc
+    export FC=gfortran
+    export LDD="ldd"
 fi
+
+
+#if [[ $(uname) == Darwin ]]; then
+#  export LIBRARY_SEARCH_VAR=DYLD_FALLBACK_LIBRARY_PATH
+#elif [[ $(uname) == Linux ]]; then
+#  export LIBRARY_SEARCH_VAR=LD_LIBRARY_PATH
+#fi
 
 
 #export LDFLAGS="-L${PREFIX}/lib"
 #export CFLAGS="${CFLAGS} -I${PREFIX}/include"
 
-# Test suite
-# tests are performed during building as they are not available in the
-# installed package.
-# Additional tests can be run with "make smallcheck" and "make bigcheck"
-#TEST_CMD="eval cd tests && ${LIBRARY_SEARCH_VAR}=\"$PREFIX/lib\" make check-local && cd -"
 
-export FC=gfortran-5
-export CC=gcc-5
-
+# https://github.com/ContinuumIO/anaconda-issues/issues/739
 export LDFLAGS="$LDFLAGS -L$PREFIX/lib -Wl,-rpath,$PREFIX/lib"
 
-#export CFLAGS="-g -O0"  
+# See https://github.com/Homebrew/legacy-homebrew/issues/20233
+# For install_name_tool to work when the install names are larger the binary should be built with 
+# the ld(1) -headerpad_max_install_names option.
+export FC_LDFLAGS_EXTRA="-Wl,-headerpad_max_install_names"
+
 export CFLAGS="$CFLAGS -g -O0 -fPIC -I$PREFIX/include"
-#export FCFLAGS="-O0 -g -ffree-line-length-none" 
 export FCFLAGS="-O0 -g -ffree-line-length-none -Wl,-rpath,${CONDA_PREFIX}/lib" 
 # -fPIC or -fpic
 # see https://gcc.gnu.org/onlinedocs/gcc-4.8.3/gcc/Code-Gen-Options.html#Code-Gen-Options
 
+
+LIBGFORTRAN_PATH=/usr/local/opt/gcc/lib/gcc/5/libgfortran.3.${DYLIB_EXT} 
+LIBGCC_S_PATH=/usr/local/lib/gcc/5/libgcc_s.1.${DYLIB_EXT} 
+LIBQUADMATH_PATH=/usr/local/opt/gcc/lib/gcc/5/libquadmath.0.${DYLIB_EXT} 
+
 mkdir ${PREFIX}/lib
-cp ~/anaconda2/lib/libgfortran.3.dylib ${PREFIX}/lib/libgfortran.3.dylib
-cp ~/anaconda2/lib/libquadmath.0.dylib ${PREFIX}/lib/libquadmath.0.dylib 
-cp ~/anaconda2/lib/libgcc_s.1.dylib  ${PREFIX}/lib/libgcc_s.1.dylib
+cp ~/anaconda2/lib/libgfortran.3.${DYLIB_EXT} ${PREFIX}/lib/libgfortran.3.${DYLIB_EXT}
+cp ~/anaconda2/lib/libquadmath.0.${DYLIB_EXT}  ${PREFIX}/lib/libquadmath.0.${DYLIB_EXT} 
+cp ~/anaconda2/lib/libgcc_s.1.${DYLIB_EXT}   ${PREFIX}/lib/libgcc_s.1.${DYLIB_EXT} 
 
 ./configure --prefix=${PREFIX} --enable-mpi=no --with-linalg-flavor=none --with-fft-flavor=none
 make -j${CPU_COUNT}
+
+
+# Test suite
+# tests are performed during building as they are not available in the
+# installed package.
+#make check
+
 make install-exec
 
-#install_name_tool -change @rpath/./libgfortran.3.dylib ${CONDA_PREFIX}/lib/libgfortran.3.dylib \
-#                  -change @rpath/./libquadmath.0.dylib ${CONDA_PREFIX}/lib/libquadmath.0.dylib \
-#                  -change @rpath/./libgcc_s.1.dylib  ${CONDA_PREFIX}/lib/libgcc_s.1.dylib \
-#                  $PREFIX/bin/abinit
 
-LIBGFORTRAN_PATH=/usr/local/opt/gcc/lib/gcc/5/libgfortran.3.dylib
-LIBGCC_S_PATH=/usr/local/lib/gcc/5/libgcc_s.1.dylib
-LIBQUADMATH_PATH=/usr/local/opt/gcc/lib/gcc/5/libquadmath.0.dylib
+declare -a BINARIES=(
+"abinit" "band2eps" "cut3d" "ioprof" "mrgdv" "ujdet"
+"bsepostproc" "lapackprof" "mrggkk" "vdw_kernelgen"
+"aim" "fftprof" "macroave" "mrgscr" 
+"anaddb" "conducti" "fold2Bloch" "mrgddb" "optic"
+)
 
-otool -L $PREFIX/bin/abinit
-install_name_tool \
-  -change ${LIBGCC_S_PATH} ${CONDA_PREFIX}/lib/libgcc_s.1.dylib \
-  -change ${LIBQUADMATH_PATH} ${CONDA_PREFIX}/lib/libquadmath.0.dylib \
-  -change ${LIBGFORTRAN_PATH} ${CONDA_PREFIX}/lib/libgfortran.3.dylib \
-  $PREFIX/bin/abinit
-#otool -L $PREFIX/bin/abinit
+if [[ `uname` == 'Darwin' ]]; then
+    for bname in "${BINARIES[@]}" do
+        $LDD $PREFIX/bin/${bname}
+        install_name_tool \
+          -change ${LIBGCC_S_PATH} ${CONDA_PREFIX}/lib/libgcc_s.1.${DYLIB_EXT} \
+          -change ${LIBQUADMATH_PATH} ${CONDA_PREFIX}/lib/libquadmath.0.${DYLIB_EXT} \
+          -change ${LIBGFORTRAN_PATH} ${CONDA_PREFIX}/lib/libgfortran.3.${DYLIB_EXT} \
+          $PREFIX/bin/${bname}
+        #$LDD $PREFIX/bin/${bname}
+    done 
+fi 
 
 unset FC
 unset CC
 unset CFLAGS
 unset FCFLAGS
+unset LDFLAGS
+unset FC_LDFLAGS_EXTRA
