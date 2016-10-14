5392d0e28a7462fde7514944f9d0f4944b268b39
gmatteo <gmatteo@gmail.com>
2016-10-14 17:02:52 +0200

    Use variables for DIRS and BASENAMES

--- recipes/abinit/build.sh
+++ recipes/abinit/build.sh
@@ -4,7 +4,6 @@
 # Depending on our platform, shared libraries end with either .so or .dylib
 if [[ `uname` == 'Darwin' ]]; then
     export DYLIB_EXT=dylib
-    #export CC=clang
     export CC=gcc-5
     export FC=gfortran-5
     export LDD="otool -L"
@@ -12,19 +11,8 @@ else
     export DYLIB_EXT=so
     export CC=gcc
     export FC=gfortran
-    export LDD="ldd"
+    export LDD=ldd
 fi
-
-
-#if [[ $(uname) == Darwin ]]; then
-#  export LIBRARY_SEARCH_VAR=DYLD_FALLBACK_LIBRARY_PATH
-#elif [[ $(uname) == Linux ]]; then
-#  export LIBRARY_SEARCH_VAR=LD_LIBRARY_PATH
-#fi
-
-
-#export LDFLAGS="-L${PREFIX}/lib"
-#export CFLAGS="${CFLAGS} -I${PREFIX}/include"
 
 
 # https://github.com/ContinuumIO/anaconda-issues/issues/739
@@ -41,14 +29,24 @@ export FCFLAGS="-O0 -g -ffree-line-length-none -Wl,-rpath,${CONDA_PREFIX}/lib"
 # see https://gcc.gnu.org/onlinedocs/gcc-4.8.3/gcc/Code-Gen-Options.html#Code-Gen-Options
 
 
-LIBGFORTRAN_PATH=/usr/local/opt/gcc/lib/gcc/5/libgfortran.3.${DYLIB_EXT} 
-LIBGCC_S_PATH=/usr/local/lib/gcc/5/libgcc_s.1.${DYLIB_EXT} 
-LIBQUADMATH_PATH=/usr/local/opt/gcc/lib/gcc/5/libquadmath.0.${DYLIB_EXT} 
+LIBGFORTRAN_DIR=/usr/local/opt/gcc/lib/gcc/5
+LIBGFORTRAN_NAME=libgfortran.3.${DYLIB_EXT} 
 
-mkdir ${PREFIX}/lib
-cp ~/anaconda2/lib/libgfortran.3.${DYLIB_EXT} ${PREFIX}/lib/libgfortran.3.${DYLIB_EXT}
-cp ~/anaconda2/lib/libquadmath.0.${DYLIB_EXT}  ${PREFIX}/lib/libquadmath.0.${DYLIB_EXT} 
-cp ~/anaconda2/lib/libgcc_s.1.${DYLIB_EXT}   ${PREFIX}/lib/libgcc_s.1.${DYLIB_EXT} 
+LIBGCC_S_DIR=/usr/local/lib/gcc/5
+LIBGCC_S_NAME=libgcc_s.1.${DYLIB_EXT} 
+
+LIBQUADMATH_DIR=/usr/local/opt/gcc/lib/gcc/5
+LIBQUADMATH_NAME=libquadmath.0.${DYLIB_EXT} 
+
+LIBGFORTRAN_PATH=${LIBGFORTRAN_DIR}/${LIBGFORTRAN_PATH}
+LIBGCC_S_PATH=${LIBGCC_S_DIR}/${LIBGCC_S_NAME}
+
+LIBQUADMATH_PATH=${LIBQUADMATH_DIR}/${LIQUADMATH_NAME}
+
+mkdir -p ${PREFIX}/lib
+cp ~/anaconda2/lib/${LIBGFORTRAN_NAME} ${PREFIX}/lib/${LIBGFORTRAN_NAME}
+cp ~/anaconda2/lib/${LIBQUADMATH_NAME} ${PREFIX}/lib/${LIBQUADMATH_NAME}
+cp ~/anaconda2/lib/${LIBGCC_S_NAME}    ${PREFIX}/lib/${LIBGCC_S_NAME}
 
 ./configure --prefix=${PREFIX} --enable-mpi=no --with-linalg-flavor=none --with-fft-flavor=none
 make -j${CPU_COUNT}
@@ -62,7 +60,7 @@ make -j${CPU_COUNT}
 make install-exec
 
 
-declare -a BINARIES=(
+declare -a ABINIT_BINARIES=(
 "abinit" "band2eps" "cut3d" "ioprof" "mrgdv" "ujdet"
 "bsepostproc" "lapackprof" "mrggkk" "vdw_kernelgen"
 "aim" "fftprof" "macroave" "mrgscr" 
@@ -70,14 +68,15 @@ declare -a BINARIES=(
 )
 
 if [[ `uname` == 'Darwin' ]]; then
-    for bname in "${BINARIES[@]}" do
-        $LDD $PREFIX/bin/${bname}
+    for bname in "${ABINIT_BINARIES[@]}" 
+    do
+        #${LDD} $PREFIX/bin/${bname}
         install_name_tool \
-          -change ${LIBGCC_S_PATH} ${CONDA_PREFIX}/lib/libgcc_s.1.${DYLIB_EXT} \
-          -change ${LIBQUADMATH_PATH} ${CONDA_PREFIX}/lib/libquadmath.0.${DYLIB_EXT} \
-          -change ${LIBGFORTRAN_PATH} ${CONDA_PREFIX}/lib/libgfortran.3.${DYLIB_EXT} \
+          -change ${LIBGCC_S_PATH} ${CONDA_PREFIX}/lib/${LIBGCC_S_NAME} \
+          -change ${LIBQUADMATH_PATH} ${CONDA_PREFIX}/lib/${LIBQUADMATH_NAME} \
+          -change ${LIBGFORTRAN_PATH} ${CONDA_PREFIX}/lib/${LIBQUADMATH_NAME} \
           $PREFIX/bin/${bname}
-        #$LDD $PREFIX/bin/${bname}
+        #${LDD} $PREFIX/bin/${bname}
     done 
 fi 
 
